ad24617fff0e969722bc94776f82f797d30dbe73
larry2310 <larryxfactor@gmail.com>
2023-09-04 13:12:56 -0700

    0-create_array.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,13 +1,16 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-char *create_array(unsigned int size, char c)
-{
-	char *a = malloc(size * sizeof(char));
-	unsigned int i;
+#include <stdio.h>
+#include <stdlib.h>
 
-	if (size == 0 || a == 0)
-		return (NULL);
-	for (i = 0 ; i < size ; i++)
-		a[i] = c;
-	return (a);
-}
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
+#endif
